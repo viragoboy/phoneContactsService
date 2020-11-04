11091a82df447e15c2b784014492f7ede0c01c0b
Carl Domingue <gitlab@dsixe.com>
2020-11-04 12:33:14 -0500

    updated json

--- index.js
+++ index.js
@@ -6,14 +6,36 @@ const bodyparser = require("body-parser");
 const port = process.env.PORT || 3200;
 
 const contacts = [{
-    name: 'bob mcnamara',
-    phone: '5551212'
+  "name": "Carl",
+  "address": {
+    "street": "Ruby",
+    "city": "Dawn",
+    "state": "Ga",
+    "zip": "30534"
   },
-  {
-    name: 'susan mcnamara',
-    phone: '5553344'
-  }
-];
+  "phone": "6785551212",
+  "isNewContact": false
+}, {
+  "name": "Taty",
+  "address": {
+    "street": "Lane",
+    "city": "Ville",
+    "state": "Ga",
+    "zip": "30477"
+  },
+  "phone": "4703187267",
+  "isNewContact": false
+}, {
+  "name": "Lila",
+  "address": {
+    "street": "Rock",
+    "city": "Garden",
+    "state": "Ga",
+    "zip": "77422"
+  },
+  "phone": "7707707700",
+  "isNewContact": false
+}];
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({
@@ -25,6 +47,7 @@ app.listen(port, () => {
 });
 
 app.get("/getAllContacts", (req, res) => {
+  res.type('application/json')
   res.status(200).send(contacts);
 });
 
